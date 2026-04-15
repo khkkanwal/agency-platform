edd3a58571b3e1ae1bec876c887aaa02daf5f3cc
Kanwal Khalid <kanwalkhalid09@gmail.com>
2026-04-15 12:24:22 +0500

    testing Api's

--- server/routes/portfolioRoutes.js
+++ server/routes/portfolioRoutes.js
@@ -1,17 +1,17 @@
 import express from "express";
-import serviceController from "../controllers/serviceController.js";
+import portfolioController from "../controllers/portfolioController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 // Create a new portfolio item
-router.post("/", protect, serviceController.createPortfolio);
+router.post("/", protect, portfolioController.createPortfolio);
 // Get all portfolio items
-router.get("/", serviceController.getAllPortfolios);
+router.get("/", portfolioController.getAllPortfolios);
 // Get a single portfolio item by ID
-router.get("/:id", serviceController.getPortfolioById);
+router.get("/:id", portfolioController.getPortfolioById);
 // Update a portfolio item by ID
-router.put("/:id", protect, serviceController.updatePortfolio);
+router.put("/:id", protect, portfolioController.updatePortfolio);
 // Delete a portfolio item by ID
-router.delete("/:id", protect, serviceController.deletePortfolio);
+router.delete("/:id", protect, portfolioController.deletePortfolio);
 export default router;
